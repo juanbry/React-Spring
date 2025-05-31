f6b5a7f3a5e4a827776b1b1ac8d8b091677c19b9
Joaquin12DM <joaquindm1226@gmail.com>
2025-05-31 00:11:33 -0500

    version base springboot

---
+++ AppEstudio/src/main/java/com/aplicacion/appestudio/AppEstudioApplication.java
@@ -0,0 +1,13 @@
+package com.aplicacion.appestudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEstudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppEstudioApplication.class, args);
+    }
+
+}
